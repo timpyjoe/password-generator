ebaa82bae98c08e66bab20b7db947ab88e78ed9b
Tim Poffenberger <timpoffenberger23@gmail.com>
2023-09-30 15:48:12 -0500

    Adds password lenght prompt and placeholder strings for types of characters to be included

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Empty array that will be populated as the user decides what to include in their password
+var passOptions = [];
+
+// Placeholders for all different options that can be included in the password
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+var specialChar = " !#%$&'()*+,-./:;<=>?@[\]^_`{|}~";
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,6 +17,12 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+// Generates the password
+function generatePassword() {
+  var passLength = prompt("How long would you like your password to be? (8-20)");
+  passLength = passLength * 1;
 }
 
 // Add event listener to generate button
