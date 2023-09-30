de61bdddade4aa149781b1e7195bd85744110621
Tim Poffenberger <timpoffenberger23@gmail.com>
2023-09-30 16:11:34 -0500

    rearranges functions and adds protection agains invalid passworld length entries

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Empty array that will be populated as the user decides what to include in their password
-var passOptions = [];
+// Empty variable that will be populated as the user decides what to include in their password
+var passOptions = "";
 
 // Placeholders for all different options that can be included in the password
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "1234567890";
 var specialChar = " !#%$&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+// Determines the password length
+function passwordLength() {
+  var passLength = prompt("How long would you like your password to be? (8-20)");
+  passLength = passLength * 1;
+  console.log(typeof passLength);
+  if (passLength < 8 || passLength > 20) {
+    alert("Please enter a valid password length.");
+    passwordLength();
+  }
+  else if (Number.isNaN(passLength)) {
+    alert("Please enter a number.");
+    passwordLength();
+  }
+}
+
+// Generates the password
+function generatePassword() {
+  passwordLength();
+
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -17,12 +38,6 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
-
-// Generates the password
-function generatePassword() {
-  var passLength = prompt("How long would you like your password to be? (8-20)");
-  passLength = passLength * 1;
 }
 
 // Add event listener to generate button
